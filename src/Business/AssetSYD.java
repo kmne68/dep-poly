3c0485b32c7c17453f86fe68b18612757afe735a
kmne68 <kmne68@gmail.com>
2017-02-15 20:04:08 -0600

    WIP add build method to assetSYD class.

--- src/Business/AssetSYD.java
+++ src/Business/AssetSYD.java
@@ -10,85 +10,125 @@ package Business;
  * @author Keith
  */
 abstract public class AssetSYD extends Asset {
-    
+
     private double[] beginningBalance;
     private double[] annualDepreciation;
     private double[] endingBalance;
     private double[] annualRate;
     private boolean built;
-    
+
     public static final double FACTOR = 2.0;
-    
+
     public AssetSYD() {
-        super();   
+        super();
         this.built = false;
     }
 
     public AssetSYD(String name, double cost, double salvage, int life) {
-        super(name, cost, salvage, life);        
-        
+        super(name, cost, salvage, life);
+
         if (super.isValid()) {
             build();
         }
     }
-    
-    
+
     private void build() {
-        // remove this loop for program
-        for ( int i = 0; i < super.getLifeOfItem(); i++) {
-            this.beginningBalance[i] = Math.random() * 100.0;
-            this.annualDepreciation[i] = Math.random() * 100.0;
-            this.endingBalance[i] = Math.random() * 100.0;
-            this.annualRate[i] = Math.random() * 100.0;
+        
+        try {
+            this.beginningBalance = new double[super.getLifeOfItem()];
+            this.endingBalance = new double[super.getLifeOfItem()];
+            this.annualDepreciation = new double[super.getLifeOfItem()];
+            this.annualRate = new double[super.getLifeOfItem()];
+            
+            int syd = super.getLifeOfItem() * (super.getLifeOfItem() + 1) / 2;
+            double depreciationTotal = super.getAssetCost()- super.getSalvageValue();
+            this.beginningBalance[0] = super.getAssetCost();
+            
+            for(int year = 0; year < super.getLifeOfItem(); year++) {
+                if(year > 0) {
+                    this.beginningBalance[year] = this.endingBalance[year - 1];
+                }
+                
+                
+                int remainingLife = super.getLifeOfItem() - year;   // numerator
+                this.annualRate[year] = (double)remainingLife / (double)syd;
+                this.annualDepreciation[year] = depreciationTotal * this.annualRate[year];
+                this.endingBalance[year] = this.beginningBalance[year] - this.annualDepreciation[year];
+             }
+        } catch (Exception e) {
+            super.setErrorMessage("SYD build failed" + e.getMessage());
+            this.built = false;
         }
-        this.built = true;
     }
     
-    
+/*    private void build() {
+
+        if (!super.isValid()) {
+            this.built = false;
+        } else {
+            try {
+                this.beginningBalance = new double[super.getLifeOfItem()];
+                this.endingBalance = new double[super.getLifeOfItem()];
+                this.annualDepreciation = new double[super.getLifeOfItem()];
+                
+                this.beginningBalance[0] = super.getAssetCost();
+                for (int year = 0; year < super.getLifeOfItem(); year++) {
+
+                    if(year > 0) {
+                        this.beginningBalance[year] = this.endingBalance[year - 1];
+                    }
+                    this.annualRate[year + 1] = year + 1 / (((year + 1) * year + 2) / 2);
+                    
+                    this.annualDepreciation[year] = this.beginningBalance[year] * this.annualRate[year];
+                    this.endingBalance[year] = this.beginningBalance[year] * this.annualRate[year];
+                }
+            } catch (Exception e) {
+                
+            }
+            this.built = true;
+        }
+    } */
+
     @Override
     public double getBeginningBalance(int year) {
-        
-        if(!this.built) {
+
+        if (!this.built) {
             build();
-            if(!this.built) {
+            if (!this.built) {
                 return -1;
             }
         }
         return this.beginningBalance[year - 1];
     }
-    
-    
+
     @Override
     public double getAnnualDepreciation(int year) {
-        
-        if(!this.built) {
+
+        if (!this.built) {
             build();
-            if(!this.built) {
+            if (!this.built) {
                 return -1;
             }
         }
         return this.annualDepreciation[year - 1];
     }
-    
-    
-    
+
     @Override
     public double getEndingBalance(int year) {
-        if(!this.built) {
+        if (!this.built) {
             build();
-            if(!this.built) {
+            if (!this.built) {
                 return -1;
             }
         }
         return this.endingBalance[year - 1];
     }
-    
-    
+
     public double getAnnualRate(int year) {
-        
-        if(!this.built) {
+
+        if (!this.built) {
             build();
-            if(!this.built) {
+            if (!this.built) {
                 return -1;
             }
         }
